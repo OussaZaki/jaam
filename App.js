6174213ec531b916259f9b8f7d379750cf2bc2de
Oussama Zaki <oussama.zaki@klarna.com>
2019-05-26 23:38:15 +0200

    🚕 setup base navigation

--- App.js
+++ App.js
@@ -1,62 +1,10 @@
-import React from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { createStackNavigator, createAppContainer } from "react-navigation";
+import HomeScreen from "./screens/Home";
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <View style={styles.spacer}></View>
-        <View style={styles.header}>
-          <Text style={styles.title}>Welcome to Jaam!</Text>
-        </View>
-        <View style={styles.content}>
-          <Text style={styles.description}>Let's start by connecting your Spotify account!</Text>
-        </View>
-        <View style={styles.footer}>
-          <TouchableOpacity onPress={console.log}>
-            <Text style={styles.spotifyButton}>Connect Spotify</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center"
-  },
-  spacer: {
-    flex: 4
-  },
-  header: {
-    flex: 2,
-  },
-  title: {
-    fontSize: 28,
-    fontWeight: "bold"
-  },
-  content: {
-    flex: 8,
-  },
-  description: {
-    fontSize: 18,
-    textAlign: "center"
-  },
-  footer: {
-    flex: 4
-  },
-  spotifyButton: {
-    paddingVertical: 10,
-    paddingHorizontal: 40,
-    backgroundColor: "#1DB954",
-    color: "white",
-    borderRadius: 50,
-    fontSize: 16,
-    textTransform: "uppercase",
-    letterSpacing: 2,
-    fontWeight: "500"
-  }
+const MainNavigator = createStackNavigator({
+  Home: { screen: HomeScreen }
 });
+
+const App = createAppContainer(MainNavigator);
+
+export default App;
