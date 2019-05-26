01f270fd81854b8ec1e24360bd3fb6d7599f6bbc
Oussama Zaki <oussama.zaki@klarna.com>
2019-05-26 23:40:04 +0200

    🚕 remove navigation header

--- App.js
+++ App.js
@@ -1,9 +1,17 @@
 import { createStackNavigator, createAppContainer } from "react-navigation";
 import HomeScreen from "./screens/Home";
 
-const MainNavigator = createStackNavigator({
-  Home: { screen: HomeScreen }
-});
+const MainNavigator = createStackNavigator(
+  {
+    Home: { screen: HomeScreen }
+  },
+  {
+    headerMode: "none",
+    navigationOptions: {
+      headerVisible: false
+    }
+  }
+);
 
 const App = createAppContainer(MainNavigator);
 
