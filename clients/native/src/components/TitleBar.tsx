7641983302bd15ab47d0ea39545bf6c063ede6bd
Oussama Zaki <oussa@Oussamas-MBP.localdomain>
2020-02-07 14:21:20 +0100

    🏷 Add primary title with underline bar

---
+++ clients/native/src/components/TitleBar.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Colors } from './Colors';
+
+const DEFAULT_BAR_WIDTH = 42
+
+type StyleProps = {
+    width?: number;
+}
+
+const getStyles = (style: StyleProps) => StyleSheet.create({
+    titleBar: {
+        backgroundColor: Colors.primary,
+        width: style.width || DEFAULT_BAR_WIDTH,
+        height: 6,
+        marginTop: 12
+    }
+})
+
+type TitleBarProps = StyleProps;
+
+export const TitleBar: React.SFC<TitleBarProps> = props => {
+    const styles = getStyles(props);
+
+    return (
+        <View style={styles.titleBar}></View>
+    )
+}
+
+export default TitleBar;
---
+++ clients/native/src/components/TitlePrimary.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, Text, TextProps } from 'react-native';
+import { Colors } from './Colors';
+import { Fonts } from './Fonts';
+
+const styles = StyleSheet.create({
+    titlePrimary: {
+        fontFamily: Fonts.bold,
+        color: Colors.primary,
+        fontSize: 42,
+        flexShrink: 1
+    }
+})
+
+type TitlePrimaryProps = TextProps & {
+    text: string;
+}
+
+export const TitlePrimary: React.SFC<TitlePrimaryProps> = props => (
+    <Text style={styles.titlePrimary} {...props}>
+        {props.text}
+    </Text>
+)
+
+export default TitlePrimary;
