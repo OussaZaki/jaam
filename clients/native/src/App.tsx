e183f14ac36fe03468f3945d8edb900764498813
Oussama Zaki <oussa@Oussamas-MBP.localdomain>
2020-02-07 14:22:04 +0100

    🎹 Add playlist screen

--- clients/native/src/App.tsx
+++ clients/native/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import LoginScreen from './screens/Login';
+import PlaylistsScreen from './screens/Playlists';
 
 
 const App = () => {
   return (
     <>
       <LoginScreen />
+      {/* <PlaylistsScreen /> */}
     </>
   );
 }
